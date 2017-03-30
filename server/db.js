12f2d0ec42f4cbe714f8b8bfd8cd4817e51f77e0
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2017-03-30 19:31:35 +0000

    fixed settings.sh

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 var Sequelize = require('sequelize');
-var DatabaseSettings = require('../settings.js');
+//var DatabaseSettings = require('../settings.js');
 
 //connect to Heroku PostGres Database
 // var db = new Sequelize("postgres://xhzrrvvawqzqov:1c2ac54a72b223e5d603ce03d8195194ac39336f544c04b266758f083aea4a15@ec2-23-23-228-115.compute-1.amazonaws.com:5432/d5lfn726hf4pp6?ssl=true", {"dialect":"postgres", "ssl":true, "dialectOptions":{"ssl":{"require":true}}});
 
-var db = new Sequelize(process.env.DATABASE_URL || DatabaseSettings.DATABASE_URL, {
+var db = new Sequelize(process.env.DATABASE_URL || DATABASE_URL, {
   dialect: 'postgres',
   ssl: true,
   dialectOptions: {
